8fe23bb79a9dc42e322b485f51e4398283476a90
mister_name <36573936+mistername@users.noreply.github.com>
2020-04-28 14:38:42 +0200

    changed param to ShaderDecompiler.exe input output

--- csharp/ShaderDecompiler/Entry.cs
+++ csharp/ShaderDecompiler/Entry.cs
@@ -13,44 +13,16 @@ namespace ShaderDecompiler
     {
         public static void Main(string[] args)
         {
-            Array.Resize(ref args, 2);
-            //args[0] = "hgwaterfall.shader";
-            //args[1] = "hgwaterfallNEW.shader";
-            //if (args.Length != 2)
-            //{
-            //    Console.WriteLine("shaderReconstructor inputFile outputFile");
-            //    return;
-            //}
-
-            //string input = args[0];
-            //string output = args[1];
-
-            var inputs = Directory.GetFiles(Path.GetDirectoryName(Assembly.GetAssembly(typeof(Entry)).Location), "*.shader", SearchOption.AllDirectories);
-
-            foreach (var input in inputs)
+            if (args.Length != 2)
             {
-                Console.WriteLine(input);
+                Console.WriteLine("shaderReconstructor inputFile outputFile");
+                return;
             }
 
-            Console.ReadLine();
+            string input = args[0];
+            string output = args[1];
 
-            foreach (var file in inputs)
-            {
-                Console.Title = Path.GetFileNameWithoutExtension(file);
-                var output = Path.GetFileNameWithoutExtension(file) + "_new" + ".shader";
-                try
-                {
-                    ShaderProcess(file, output);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("exception on shader " + file + ":\n" + e);
-                    if (System.IO.File.Exists(output))
-                    {
-                        System.IO.File.Delete(output);
-                    }
-                }
-            }
+            ShaderProcess(input,output);
         }
 
         private static void ShaderProcess(string input, string output)
